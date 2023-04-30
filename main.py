7febbb0201bec397d8e9e905fdcec132f3ed1aed
Maio39 <marcomaier3903@gmail.com>
2023-04-30 21:29:40 +0200

    Lab1: String

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+#hey.. there is nothing here. WAKE UP!!!
+
 print("Hello World")
+print("I am a Iron Man")
+print("No, I am a Spiderman")
+
+#We are practicing Python..... IT'S AWESOME!!!!
+
+#multiline string
+print("""Hello world
+I am a Iron Man
+No, I am a Spiderman""")
+
+#concat string
+print("Hello World. \n" + "I am a Iron Man. \n" + "No, I Am a Spiderman")
+
+#
+print("I am Poppy\n" * 100)
