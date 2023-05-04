2a65c2f985d823a763406d54c9b017e6fda4979d
Maio39 <marcomaier3903@gmail.com>
2023-05-04 12:37:10 +0200

    Lab 10: Tuple

---
+++ lab10.py
@@ -0,0 +1,18 @@
+# list vs tuple
+
+alist = ["Bernard", "hackwell", 92]
+
+#This works
+alist[0] = "Marco"
+
+atuple = ("Bernard", "hackwell", 92)
+
+#This not works
+#atuple[0] = "Marco"
+
+#tuple
+Marco = ("Maio",20,"developer")
+(name,age,job) = Marco
+print(name)
+print(age)
+print(job)
